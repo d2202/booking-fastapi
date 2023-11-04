e51b7cdcd83588b9420f65d533bd07acd71c8ad9
Denis Besfamilnyy <besfamilnyy.wrk@gmail.com>
2023-11-04 15:05:48 +0300

    added get_by_id method in Base repo

--- app/src/repositories/base_repository.py
+++ app/src/repositories/base_repository.py
@@ -7,6 +7,15 @@ class BaseRepository:
     model = None
 
     @classmethod
+    async def get_by_id(cls, model_id):
+        if cls.model is None:
+            raise AttributeError("Attribute 'model' is None")
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(id=model_id)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
     async def get_all(cls):
         if cls.model is None:
             raise AttributeError("Attribute 'model' is None")
