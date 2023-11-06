f54cf7bcec6bdc4a5739fa9f845fd08cd6a184ab
Denis Besfamilnyy <besfamilnyy.wrk@gmail.com>
2023-11-06 23:14:13 +0300

    added missing files to previous commit

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.src.api.bookings_router import router as router_booking
+from app.src.api.auth_router import router as router_users
 
-app = FastAPI(debug=True)
+app = FastAPI()
+app.include_router(router=router_users)
 app.include_router(router=router_booking)
 
 
--- app/src/repositories/base_repository.py
+++ app/src/repositories/base_repository.py
@@ -1,3 +1,5 @@
+from typing import Self, Any
+
 from sqlalchemy import select
 
 from app.src.models.db import async_session_maker
@@ -7,7 +9,7 @@ class BaseRepository:
     model = None
 
     @classmethod
-    async def get_by_id(cls, model_id):
+    async def get_by_id(cls, model_id: int) -> Self:
         if cls.model is None:
             raise AttributeError("Attribute 'model' is None")
         async with async_session_maker() as session:
@@ -16,9 +18,25 @@ class BaseRepository:
             return result.scalar_one_or_none()
 
     @classmethod
-    async def get_all(cls):
+    async def find_one_or_none(cls, **filter_by: Any):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+        return result.scalar_one_or_none()
+
+    @classmethod
+    async def get_all(cls) -> Self:
         if cls.model is None:
             raise AttributeError("Attribute 'model' is None")
         async with async_session_maker() as session:
             query = select(cls.model)
             return (await session.execute(query)).scalars().all()
+
+    @classmethod
+    async def add(cls, **data: Any) -> None:
+        if cls.model is None:
+            raise AttributeError("Attribute 'model' is None")
+        new_instance = cls.model(**data)
+        async with async_session_maker() as session:
+            session.add(new_instance)
+            await session.commit()
